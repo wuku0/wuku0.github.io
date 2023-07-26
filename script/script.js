206cc28067e3e3a954612f2e966d740f528d5389
wuku <80927421+wuku0@users.noreply.github.com>
2023-07-26 21:17:43 +0200

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,3 +1,43 @@
+function showDate() {
+  var today = new Date();
+  var date = today.getDate();
+  var month = today.getMonth() + 1;
+  var year = today.getFullYear();
+  var monthName = today.toLocaleString('default', { month: 'short' });
+  var dateString = date + "." + monthName + "." + year + "";
+  return dateString;
+}
+
+var dateElement = document.getElementById("date");
+var currentDate = showDate();
+dateElement.innerHTML = currentDate;
+
+//GET STEAM RICH PRESENCE
+async function scrapeSteamProfile() {
+  const proxyURL = 'https://cors-anywhere.herokuapp.com/';
+  const steamProfileURL = 'https://steamcommunity.com/id/sirwuku';
+  try {
+    const response = await fetch(proxyURL + steamProfileURL);
+    const html = await response.text();
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(html, 'text/html');
+    const xpath = '/html/body/div[1]/div[7]/div[6]/div[1]/div[2]/div/div[1]/div[1]/div/div[2]';
+    const element = doc.evaluate(xpath, doc, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+    if (element) {
+      const info = element.textContent;
+      document.getElementById('result').textContent = info + "     on Steam";
+    } else {
+      document.getElementById('result').textContent = "Nothing.";
+    }
+  } catch (err) {
+    console.error('Error:', err);
+    document.getElementById('result').textContent = 'An error occurred. Please check the console.';
+  }
+}
+  window.onload = scrapeSteamProfile;
+
+
+
 //WUKU_
 const wuku = document.getElementById('wuku');
 const letters = ['w', 'u', 'k', 'u'];
